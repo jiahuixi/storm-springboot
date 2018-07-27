0be826ce648ff54fb99d682cec01d11b024f282b
jiahuixi <791226357@qq.com>
2018-07-27 16:22:15 +0800

    111111111111111111111111111111111111

--- src/main/java/hytera/storm/Application.java
+++ src/main/java/hytera/storm/Application.java
@@ -1,8 +1,10 @@
 package hytera.storm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 
@@ -14,10 +16,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 * @date 2018年1月5日
  */
 @SpringBootApplication
+@EnableTransactionManagement//事务注解
+@MapperScan("hytera.storm.dao")
 public class Application {
 	public static void main(String[] args) {
 		// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		 SpringApplication.run(Application.class, args);
 	}
 	
 }
